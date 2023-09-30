ff7c9e0408c66a4b7411349aad5dbb1c0bf1df56
Hardeep Singh <contact@hardeep-singh.com>
2023-09-30 18:50:11 +1300

    remove unused code

--- SwiftUIComponents/ProgressBarViews/LinearProgressBar/LinearProgressBar.swift
+++ SwiftUIComponents/ProgressBarViews/LinearProgressBar/LinearProgressBar.swift
@@ -52,9 +52,3 @@ struct LinearProgressBar: View {
     }
     
 }
-
-//struct LinearProgressBar_Previews: PreviewProvider {
-//    static var previews: some View {
-//        LinearProgressBar(viewModel: .init(range: 0...1.0, currentValue: 0.5))
-//    }
-//}
